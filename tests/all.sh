d0e50e68c21a773e657a02eace233dcf25414944
Gunther Klessinger <gk@axiros.com>
2019-02-07 14:49:20 +0100

    check travis header

--- tests/all.sh
+++ tests/all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "getting"
-wget --no-check-certificate "https://axchange.axiros.com/travis/`git rev-parse HEAD`"
+wget --no-check-certificate "https://axc2.axiros.com/travis/`git rev-parse HEAD`"
 
 set -e
 ./tests/test_indexer.py
